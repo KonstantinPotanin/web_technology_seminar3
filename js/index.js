af6131c446187070154892b2fd00250c1334bbef
Konstantin <pkv90@yandex.ru>
2023-07-15 13:10:46 +0500

    Решение задачи к третьему семинару

---
+++ js/index.js
@@ -0,0 +1,26 @@
+// function myFavoriteFilm() {
+//     const favoriteFilm = "бойцовский клуб";
+//     let answer = prompt("Попробуйте угадать мой любимый фильм:");
+  
+//     if (answer.toLocaleLowerCase() == favoriteFilm) {
+//         alert("Верно, это мой любимый фильм!");
+//     }   
+//     else {
+//         answer = prompt(`Неверно. Вот подсказка. В этом фильме снимаются Эдвард Нортон и Бред Питт. Попробуйте ещё раз:`);
+  
+//         if (answer.toLocaleLowerCase() == favoriteFilm) {
+//             alert("Верно, это мой любимый фильм!");
+//         } 
+//         else {
+//             alert("Не верно. Мой любимый фильм - Бойцовский клуб.");
+//         }
+//     }
+//   }
+// myFavoriteFilm();
+
+// let myArray = ['a', 'b', 'c']
+// alert(myArray)
+// myArray[0] = prompt('Введите любое слово')
+// myArray[1] = prompt('Введите любое предложение')
+// myArray[2] = prompt('Введите любую букву')
+// alert(myArray)
